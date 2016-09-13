4386450e6e0c23ba0bb6cb1f0e70eb779753350f
Nathan Carter <ncarter@bentley.edu>
2016-09-12 22:02:47 -0400

    Progress on UI and on factoring into files

---
+++ R/preferences.R
@@ -0,0 +1,86 @@
+
+#------ Loading and saving user preferences ------#
+
+# Where preferences are stored
+
+prefsFile <- '~/.simpleCMSAddin'
+
+# Functions for reading/writing preferences
+
+getPreference <- setPreference <- preferencesKeys <- NULL
+local( {
+
+  # Internal API
+
+  preferences <- list()
+  savePreferences <- function () saveRDS( preferences, prefsFile )
+  loadPreferences <- function () {
+    if ( !file.exists( prefsFile ) ) {
+      preferences <<- list(
+        students = c(),
+        shared = 'instructor',
+        grading = 'to grade',
+        teamfolder = 'team',
+        mathjax = '\\def\\Z{\\mathbb{Z}}',
+        fileInput = ''
+      )
+      savePreferences()
+    }
+    preferences <<- readRDS( prefsFile )
+  }
+  loadPreferences()
+
+  # External API
+
+  getPreference <<- function ( name ) preferences[[name]]
+  setPreference <<- function ( name, value ) {
+    preferences[[name]] <<- value
+    savePreferences()
+  }
+  preferencesKeys <<- function () names(preferences)
+
+} )
+
+# File-related functions built on the basic preferences
+
+# When debugging, it's useful to use some test data stored in this repository.
+# To do so, use the following function to turn testing mode on.
+# This makes all the file-related functions operate on the test data and folders.
+
+enableTestMode <- disableTestMode <- testModeOn <- NULL
+local( {
+
+  testMode <- FALSE
+
+  enableTestMode <<- function () testMode <<- TRUE
+  disableTestMode <<- function () testMode <<- FALSE
+  testModeOn <<- function () testMode
+
+} )
+
+studentPath <- function ( student, file ) {
+  if ( testModeOn() )
+    paste0( './test-data/', student, '/', file )
+  else
+    paste0( '~', student, '/', getPreference( 'submission' ), '/', file )
+}
+
+teamPath <- function ( student, file ) {
+  if ( testModeOn() )
+    paste0( './test-data/team1/', file )
+  else
+    paste0( '~', student, '/', getPreference( 'teamfolder' ), '/', file )
+}
+
+instructorPath <- function ( student, file )
+  paste0( getPreference( 'grading' ), '/', student, '--', file )
+
+gradedPath <- function ( student, file )
+  studentPath( student, paste0( 'graded--', file ) )
+
+studentNames <- function () {
+  if ( testModeOn() )
+    strsplit( getPreference( 'students' ), '\\s+' )[[1]]
+  else
+    c( 'student1', 'student2' )
+}
--- R/simpleCMSAddin.R
+++ R/simpleCMSAddin.R
@@ -4,11 +4,7 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-# Where preferences are stored
-
-prefsFile <- '~/.simpleCMSAddin'
-
-# Main application function
+source( 'R/preferences.R' )
 
 simpleCMSAddin <- function() {
 
@@ -19,11 +15,9 @@ simpleCMSAddin <- function() {
     "Collect an assignment",
     "Return a graded assignment"
   )
-  TESTING <- TRUE
 
   # User interface
 
-  lastPreferences <- readRDS( prefsFile )
   ui <- navbarPage (
     "Simple Course Management System",
 
@@ -41,8 +35,7 @@ simpleCMSAddin <- function() {
         column(
           4,
           h3( "Students" ),
-          HTML( '<p><label for="students">All student usernames, separated by whitespace and/or line breaks:</label></p>' ),
-          tags$textarea( id='students', rows=6, cols=40, lastPreferences$students )
+          textAreaInput( 'students', 'All student usernames, separated by whitespace and/or line breaks:', getPreference( 'students' ) )
         ),
 
         # Folders column
@@ -50,11 +43,11 @@ simpleCMSAddin <- function() {
         column(
           4,
           h3( "Folders" ),
-          textInput( 'grading', 'Folder for importing files to grade:', lastPreferences$grading ),
+          textInput( 'grading', 'Folder for importing files to grade:', getPreference( 'grading' ) ),
           textInput( 'individual', 'Subfolder of each student home folder shared with you:',
-                     lastPreferences$individual ),
-          textInput( 'team', 'Subfolder of some student home folders shared with team:',
-                     lastPreferences$team )
+                     getPreference( 'individual' ) ),
+          textInput( 'teamfolder', 'Subfolder of some student home folders shared with team:',
+                     getPreference( 'teamfolder' ) )
         ),
 
         # MathJax column
@@ -62,8 +55,7 @@ simpleCMSAddin <- function() {
         column(
           4,
           h3( "MathJax" ),
-          HTML( '<p><label for="mathjax">MathJax definitions to import into all worksheets:</label></p>' ),
-          tags$textarea( id='mathjax', rows=6, cols=40, lastPreferences$mathjax )
+          textAreaInput( 'mathjax', 'MathJax definitions to import into all worksheets:', getPreference( 'mathjax' ) )
         )
       )
     ),
@@ -81,19 +73,21 @@ simpleCMSAddin <- function() {
         column(
           4,
           uiOutput( 'filePrompt' ),
-          textInput( 'fileInput', '' )
+          textInput( 'fileInput', '', getPreference( 'fileInput' ) ),
+          textOutput( 'fileStatus' )
         ),
         column(
           4,
-          actionButton( 'copySafe', 'Distribute (no overwriting)' ),
-          actionButton( 'copyOver', 'Distribute (overwriting)' )
+          uiOutput( 'copySafeUI' ),
+          br(),
+          uiOutput( 'copyOverUI' )
         )
       ),
       hr(),
       fluidRow(
         id = "headingRow",
         column( 5, h6( 'Student folder' ) ),
-        column( 2, h6( 'Contains a copy?' ) ),
+        column( 2, h6( 'File exists?' ) ),
         column( 3, h6( 'Last modified' ) ),
         column( 2, h6( 'Action' ) )
       )
@@ -110,64 +104,51 @@ simpleCMSAddin <- function() {
   # Event handlers for the UI
 
   server <- function ( input, output, session ) {
-
-    # Loading and saving user preferences
-
-    preferences <- list()
-    savePreferences <- function () { saveRDS( preferences, prefsFile ) }
-    loadPreferences <- function () {
-      if ( !file.exists( prefsFile ) ) {
-        preferences <<- list(
-          students = c(),
-          shared = 'instructor',
-          grading = 'to grade',
-          teamfolder = 'team',
-          mathjax = '\\def\\Z{\\mathbb{Z}}'
-        )
-        savePreferences()
-      }
-      preferences <<- readRDS( prefsFile )
-    }
-    setPreference <- function ( name, value ) {
-      preferences[[name]] <<- value
-      savePreferences()
-    }
-    loadPreferences()
-
-    # Compute paths for filesystem utilities
-
-    if ( TESTING ) {
-      studentPath <- function ( student, file ) { paste0( '~', student, '/', preferences$submission, '/', file ) }
-      teamPath <- function ( student, file ) { paste0( '~', student, '/', preferences$teamfolder, '/', file ) }
-      instructorPath <- function ( student, file ) { paste0( preferences$grading, '/', student, '--', file ) }
-      gradedPath <- function ( student, file ) { studentPath( student, paste0( 'graded--', file ) ) }
-    } else {
-      studentPath <- function ( student, file ) { paste0( './test-data/', student, '/', file ) }
-      teamPath <- function ( student, file ) { paste0( './test-data/team1/', file ) }
-      instructorPath <- function ( student, file ) { paste0( preferences$grading, '/', student, '--', file ) }
-      gradedPath <- function ( student, file ) { studentPath( student, paste0( 'graded--', file ) ) }
-    }
-
-    # Splitting students list
-
-    studentNames <- function () { strsplit( preferences$students, '\\s+' )[[1]] }
     # For the Settings tab
 
-    observeEvent( input$students, { setPreference( 'students', input$students ) } )
-    observeEvent( input$grading, { setPreference( 'grading', input$grading ) } )
-    observeEvent( input$individual, { setPreference( 'individual', input$individual ) } )
-    observeEvent( input$team, { setPreference( 'team', input$team ) } )
-    observeEvent( input$mathjax, { setPreference( 'mathjax', input$mathjax ) } )
+    for ( key in preferencesKeys() ) {
+      ( function ( k ) {
+        force( k )
+        observeEvent( input[[k]], { setPreference( k, input[[k]] ) } )
+      } )( key )
+    }
 
-    observeEvent( { c( input$sharingType, input$grading ) }, {
+    observeEvent( {
+      c( input$sharingType, input$grading, input$fileInput, input$students )
+    }, {
       index <- match( input$sharingType, sharingChoices )
-      if ( index == 1 )
+      if ( index == 1 ) {
         output$filePrompt <- renderUI( HTML( '<label class="control-label" for="fileInput">File to distribute (full path):</label>' ) )
-      else if ( index == 2 )
+        output$copySafeUI <- renderUI( actionButton( 'copySafe', 'Distribute (no overwriting)' ) )
+        output$copyOverUI <- renderUI( actionButton( 'copyOver', 'Distribute (with overwriting)' ) )
+        removeUI( '.contentRow', multiple = TRUE )
+        for ( student in studentNames() ) {
+          destination <- studentPath( student, basename( input$fileInput ) )
+          origExists <- file.exists( input$fileInput )
+          destExists <- file.exists( destination )
+          output$fileStatus <- renderText( if ( origExists ) paste( 'Last modified:', file.mtime( input$fileInput ) ) else 'No such file' )
+          insertUI( '#headingRow', 'afterEnd', ui = fluidRow( class = 'contentRow',
+            column( 5, p( destination ) ),
+            column( 2, p( destExists ) ),
+            column( 3, p( file.mtime( destination ) ) ),
+            column( 2, if ( destExists & origExists )
+                         actionButton( paste0( 'sendOver_', student ), 'Overwrite' )
+                       else if ( origExists )
+                         actionButton( paste0( 'sendSafe_', student ), 'Send file' )
+                       else
+                         p() )
+          ) )
+        }
+      } else if ( index == 2 ) {
         output$filePrompt <- renderUI( HTML( '<label class="control-label" for="fileInput">File to collect (no path):</label>' ) )
-      else
+        output$copySafeUI <- renderUI( actionButton( 'copySafe', 'Collect (no overwriting)' ) )
+        output$copyOverUI <- renderUI( actionButton( 'copyOver', 'Collect (with overwriting)' ) )
+      } else {
         output$filePrompt <- renderUI( HTML( paste0( '<label class="control-label" for="fileInput">File to return (from ',
-                                                     preferences$grading, '):</label>' ) ) )
+                                                     getPreference( 'grading' ), '):</label>' ) ) )
+        output$copySafeUI <- renderUI( actionButton( 'copySafe', 'Return (no overwriting)' ) )
+        output$copyOverUI <- renderUI( actionButton( 'copyOver', 'Return (with overwriting)' ) )
+      }
     } )
 
   }
